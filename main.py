f739862ac6dba17c7c3b7579bd125457217411f7
leskovets <lescovec@gmail.com>
2022-12-30 22:29:10 +0300

    loguru added

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from loader import bot
+from loguru import logger
 import handlers
 from utils.set_bot_commands import set_default_commands
 
-if __name__ == '__main__':
-    set_default_commands(bot)
-    bot.infinity_polling()
+logger.add("debug.log", format="{time} {level} {message}", level="DEBUG")
+
+
+@logger.catch()
+def main():
+
+    if __name__ == '__main__':
+        set_default_commands(bot)
+        bot.infinity_polling()
+
+
+main()
+
